bd6092ebbc85aa93902c37d4dda2b8da52daf59d
jinsuo1347 <koguma456@naver.com>
2020-10-01 23:19:04 +0900

    comit2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log(1)
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+};
+
+const handleHome = (req, res) => {
+  res.send("hello");
+};
+const profile = (req, res) => {
+  res.send("you ar on my profile");
+};
+
+app.get("/", handleHome);
+app.get("/profile", profile);
+app.listen(PORT, handleListening);
